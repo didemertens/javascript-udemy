64b4a52f375de158e2aaea3076dacd68b38c7324
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 09:01:56 +0100

    operator precedence

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-// Variables mutation and type coercion
+/*
+Operator precedence
+*/
 
-// var firstName = 'John';
-// var age = 28;
+var x,y;
+x = y = (3 + 5) * 4 - 5;
 
-// console.log(firstName + "'s age is " + age);
+console.log(x, y);
 
-// var job, isMarried;
-// job = 'teacher';
-// isMarried = false;
-
-// console.log(job + '. Is he maried? ' + isMarried)
-
-// age = 'twenty eight';
-// job = 'driver';
-
-// alert(age + job);
-
-// var lastName = prompt('What is his last name?');
-// console.log(firstName + ' ' + lastName)
-
-
-// Basic operators
-// var year, ageJohn, ageMark;
-// year = 2019;
-// ageJohn = 26;
-// ageMark = 30;
-
-
-// var johnOlder = ageJohn > ageMark;
-// console.log(johnOlder);
-
-// // typeof operator
-// console.log(typeof johnOlder);
+x+1;
+x++;
+console.log(x);
