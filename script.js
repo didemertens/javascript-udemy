16d51bf0a56395165dbe179461c8414d10723ccb
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 08:39:37 +0100

    variables

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello world (again!)');
+var firstName = 'Dide';
+
+console.log(firstName)
+
+var lastName = 'Mertens';
+var age = 26;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
