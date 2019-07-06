4f5baf91b2062a691749e07aa8d1e711a44ac4d8
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 12:30:04 +0100

    challenge 4 objects

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// objects
+// challenge 4
 
 var john = {
-  firstName : 'John',
-  lastName : 'Smith',
-  birthYear : 1990,
-  family : ['Jane', 'Mark', 'Bob', 'Emily'],
-  job : 'teacher',
-  isMarried : false,
-  calcAge : function() {
-    this.age = 2019 - this.birthYear;
-  }
+  fullName: 'John Smith',
+  mass: 70,
+  height: 1.80,
+  bmiCalc: function (){
+    this.bmi = this.mass / (this.height * this.height);
+  },
+}
+
+var mark = {
+  fullName: 'Mark Mert',
+  mass: 70,
+  height: 1.80,
+  bmiCalc: function (){
+    this.bmi = this.mass / (this.height * this.height);
+  },
+}
+
+if (john.bmiCalc() > mark.bmiCalc()) {
+  console.log(john.fullName + ' has a higher bmi of ' + john.bmi);
+} else if (john.bmi === mark.bmi){
+  console.log(john.fullName + ' and ' + mark.fullName +
+              ' both have a bmi of ' + john.bmi);
+} else{
+  console.log(mark.fullName + ' has a higher bmi of ' + mark.bmi);
 };
-
-// console.log(john.family[2]);
-
-// john.job = 'designer';
-
-// console.log(john.job);
-
-john.calcAge();
-console.log(john.age)
