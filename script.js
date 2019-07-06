f130a6f53ab24a171da45ad281f6568ae6e8b66a
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 10:09:10 +0100

    functions

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-var scoreJohn = (100 + 100 + 13) / 3
-var scoreMark = (100 + 100 + 103) / 3
-var scoreMary = (100 + 100 + 110) / 3
+// functions
 
-scoreJohn > scoreMark && scoreJohn > scoreMary ? console.log('John\'s team did better than Mark and Mary, with a score of ' + scoreJohn)
-  : scoreMark > scoreJohn && scoreMark > scoreMary ? console.log('Mark\'s team did better than John and Mary, with a score of ' + scoreMark)
-  : scoreJohn === scoreMark && scoreJohn === scoreMary ? console.log('John\'s, Mark\'s and Mary\'s team have the same score of ' + scoreJohn)
-  : scoreJohn === scoreMark ? console.log('John\'s and Mark\'s teams have the same score of ' + scoreJohn)
-  : scoreJohn === scoreMary ? console.log('John\'s and Mary\'s teams have the same score of ' + scoreJohn)
-  : scoreMary === scoreMark ? console.log('Mark\'s and Mary\'s teams have the same score of ' + scoreMark)
-  : console.log('Mary\'s team did better than John\'s and Mark\'s with a score of ' + scoreMary)
-
-if (scoreJohn > scoreMark){
-  console.log('John did better with a score of ' + scoreJohn);
-} else if (scoreJohn === scoreMark){
-  console.log('John and Mark have the same score of ' + scoreJohn);
-} else {
-  console.log('Mark did better with a score of ' + scoreMark);
+function calculateAge(birthYear) {
+  return 2019 - birthYear;
 }
+
+function yearsUntilRetirement(year, firstName) {
+  var age = calculateAge(year);
+  var retirement = 65 - age;
+  if (retirement > 0){
+  console.log(firstName + ' retires in ' + retirement + ' years.')
+  } else {
+    console.log(firstName + ' is already retired.')
+  }
+}
+
+yearsUntilRetirement(1993, 'John');
+yearsUntilRetirement(1929, 'Jane');
