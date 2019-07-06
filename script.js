f47bf1c9f56f876d6a530012fae5f1b12310dcab
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 10:24:37 +0100

    arrays

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-// function statements and expressions
+// arrays
 
-// first declaration
-// function whatDoYouDo(job,firstName){}
+var names = ['John', 'Mark', 'Jane'];
+var years = [1999, 1994, 1992];
 
-// expression
-var whatDoYouDo = function(job, firstName) {
-  switch(job) {
-    case 'teacher':
-      return firstName + ' teaches kids.'
-    case 'designer':
-      return firstName + ' creates websites.'
-    default:
-      return firstName + ' does something else.'
-  }
-}
+console.log(names.length);
 
-console.log(whatDoYouDo('teacher', 'John'));
+names[names.length-1] = 'Ben';
+
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+
+console.log(john);
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is not a designer'
+  : 'John is a designer.';
+  console.log(isDesigner);
