6565b7b699ddfd24f16ea35967c40f756632ca8a
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 09:08:46 +0100

    challenge 1

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 /*
 Operator precedence
-*/
 
 var x,y;
 x = y = (3 + 5) * 4 - 5;
 
-console.log(x, y);
-
 x+1;
 x++;
-console.log(x);
+
+*/
+
+// challenge 1
+
+var massJohn, heightJohn, massMark, heightMark;
+
+massJohn = 70;
+heightJohn = 1.70;
+massMark = 90;
+heightMark = 1.80;
+
+var bmiJohn, bmiMark;
+bmiJohn = massJohn / (heightJohn * heightJohn);
+bmiMark = massMark / (heightMark * heightMark);
+console.log(bmiMark, bmiJohn);
+
+var higherBmi = bmiMark > bmiJohn;
+console.log("Is Mark's BMI higher than John's? " + higherBmi);
