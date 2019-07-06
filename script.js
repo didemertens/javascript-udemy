8d5d8ee2aa2b334aefdd94da67ff1115df344c9a
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-06 09:17:47 +0100

    if else statements

--- script.js
+++ script.js
@@ -9,19 +9,13 @@ x++;
 
 */
 
-// challenge 1
+// if else
 
-var massJohn, heightJohn, massMark, heightMark;
+var firstName = 'John';
+var civilStatus = 'single';
 
-massJohn = 70;
-heightJohn = 1.70;
-massMark = 90;
-heightMark = 1.80;
-
-var bmiJohn, bmiMark;
-bmiJohn = massJohn / (heightJohn * heightJohn);
-bmiMark = massMark / (heightMark * heightMark);
-console.log(bmiMark, bmiJohn);
-
-var higherBmi = bmiMark > bmiJohn;
-console.log("Is Mark's BMI higher than John's? " + higherBmi);
+if (civilStatus === 'married'){
+  console.log(firstName + ' is married.');
+} else{
+  console.log(firstName + ' is not married.');
+}
