5efe6ea61efb9342ed1a1569817d782f0a5d5633
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-07 11:09:36 +0100

    challenge 5, loops and objects

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-// loops and iteration
+// challenge 5
 
-// for (var i = 0;  i < 10; i++){
-//   console.log(i)
-// };
-
-// var john = ['John', 'Smith', 'designer', 1990];
-
-// for (var i = 0; i < john.length; i++) {
-//   console.log(john[i]);
-// }
-
-// var i = 0
-// while (i < john.length) {
-//   console.log(john[i]);
-//   i++;
-// }
-
-// continue and break statement
-
-var john = ['John', 'Smith', false, 'designer', 1990];
-
-for (var i = john.length  - 1; i >= 0; i--) {
-  // if (typeof john[i] !== 'string') break;
-  console.log(john[i]);
+john = {
+  bills : [124, 48, 268, 180, 42],
+  calcTips : function(){
+    this.tips = [];
+    this.amount = [];
+    var percentage;
+    for (var i = 0; i < this.bills.length; i++) {
+      if (this.bills[i] < 50) {
+        percentage = 0.2
+      } else if (this.bills[i] > 50 && this.bills[i] < 200){
+        percentage = 0.15
+      } else{
+        percentage = 0.1
+      };
+      this.tips.push(this.bills[i]*percentage)
+      this.amount.push(this.bills[i]*percentage + this.bills[i])
+    };
+    return [this.tips, this.amount];
+  }
 }
+
+mark = {
+  bills : [77, 75, 110, 45],
+  tips : [],
+  amount : [],
+  calcTips : function(){
+    var percentage;
+    for (var i = 0; i < this.bills.length; i++) {
+      if (this.bills[i] < 100) {
+        percentage = 0.2
+      } else if (this.bills[i] > 100 && this.bills[i] < 300){
+        percentage = 0.1
+      } else{
+        percentage = 0.25
+      };
+      this.tips.push(this.bills[i]*percentage)
+      this.amount.push(this.bills[i]*percentage + this.bills[i])
+    };
+    return [this.tips, this.amount];
+  }
+}
+
+function calcAverageTip(tips){
+  var sum = 0
+  for (var i = 0; i < tips.length; i++){
+    sum += tips[i];
+  }
+  return sum / tips.length;
+}
+
+john.calcTips();
+mark.calcTips();
+
+john.average = calcAverageTip(john.tips);
+mark.average = calcAverageTip(mark.tips);
+
+if (john.average > mark.average){
+  console.log('John has paid more tips than mark, with an average of ' + calcAverageTip(john.tips));
+} else if (john.average === mark.average){
+  console.log('John and Mark have paid the same tip of ' + calcAverageTip(mark.tips));
+} else{
+  console.log('Mark has paid more tips than john with an average of ' + calcAverageTip(mark.tips));
+};
+
