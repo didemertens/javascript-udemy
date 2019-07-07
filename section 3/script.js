df877b95ba7f0f895707fb277743b9e3c4805d5f
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-07 11:59:54 +0100

    this and objects

--- section 3/script.js
+++ section 3/script.js
@@ -22,8 +22,8 @@ function foo() {
   console.log(age);
 }
 
-foo();
-console.log(age);
+// foo();
+// console.log(age);
 
 
 
@@ -34,55 +34,79 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
+
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
-}
+// function first() {
+//     var b = 'Hi!';
+//     second();
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
-}
-*/
+//     function second() {
+//         var c = 'Hey!';
+//         third();
 
+
+//      function third() {
+//        var d = 'John';
+//        console.log(a + b + c + d);
+//       }
+//   }
+// }
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+
+// calcAge(1999);
+
+// function calcAge(year){
+//   console.log(2019 - year);
+//   console.log(this);
+// }
+
+var john = {
+  name : 'John',
+  yearOfBirth: 1999,
+  calcAge : function(){
+    console.log(this);
+    console.log(2019 - this.yearOfBirth);
+
+    // function innerFunction() {
+    //   console.log(this);
+    // }
+    // innerFunction();
+  }
+}
 
 
+john.calcAge();
 
+var mike = {
+  name : 'Mike',
+  yearOfBirth : 1994,
+};
 
-
-
-
-
+mike.calcAge = john.calcAge;
+mike.calcAge();
