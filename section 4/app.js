aa13ba2b12797c19021beb8e280448e370740e20
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-08 20:38:08 +0100

    rolling dice, changing players

--- section 4/app.js
+++ section 4/app.js
@@ -33,4 +33,24 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. update round score if rolled number NOT 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else{
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.dice').style.display = 'none';
+  }
+
 });
