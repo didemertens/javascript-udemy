a6fa974ca134a913f2cd6abb92114b050831a9de
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-11 21:13:02 +0100

    primitives vs objects

--- Section 5/script.js
+++ Section 5/script.js
@@ -24,7 +24,7 @@
 
 
 // Object.create
-
+/*
 var personProto = {
   calculateAge : function() {
     console.log(2019 - this.yearOfBirth);
@@ -40,3 +40,47 @@ var jane = Object.create(personProto,
   name: { value : 'Jane' },
   yearOfBirth: { value : 1948 },
   });
+*/
+
+// Primitives vs objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+
+console.log(a, b)
+
+// objects
+var obj1 = {
+  name: 'John',
+  age: 25,
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age, obj2.age);
+
+// functions
+var age = 27;
+var obj = {
+  name: 'louise',
+  city: 'Paris',
+};
+
+function change(a,b){
+  a = 30;
+  b.city = 'London'
+}
+
+
+change(age, obj);
+console.log(age, obj.city);
+
+
+
+
+
+
+
+
+
