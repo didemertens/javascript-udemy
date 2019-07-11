9b7e43c476a41bc89610bb40f4341aac93d08ed1
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-11 21:02:12 +0100

    Object.create

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,23 +1,42 @@
-// var john {
-//   name : 'John',
-//   yearOfBirth: 1990,
-//   job: 'teacher',
+// // var john {
+// //   name : 'John',
+// //   yearOfBirth: 1990,
+// //   job: 'teacher',
+// // };
+
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
 // };
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-};
+// Person.prototype.calculateAge =  function() {
+//     console.log(2019 - this.yearOfBirth);
+//   }
 
-Person.prototype.calculateAge =  function() {
+// Person.prototype.lastName = 'Smith';
+
+// var john = new Person('John', 1990, 'designer');
+// var jane = new Person('Jane', 1999, 'teacher');
+
+// john.calculateAge();
+// console.log(john.lastName);
+
+
+// Object.create
+
+var personProto = {
+  calculateAge : function() {
     console.log(2019 - this.yearOfBirth);
   }
+}
 
-Person.prototype.lastName = 'Smith';
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
 
-var john = new Person('John', 1990, 'designer');
-var jane = new Person('Jane', 1999, 'teacher');
-
-john.calculateAge();
-console.log(john.lastName);
+var jane = Object.create(personProto,
+{
+  name: { value : 'Jane' },
+  yearOfBirth: { value : 1948 },
+  });
