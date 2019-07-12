804f0f5e93baa8e7105db8d71e130c5580783450
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-12 21:20:39 +0100

    first class functions, functions returning functions

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,34 +1,27 @@
-// first-class functions
+// functions returning functions
 
-var years = [1990, 1994, 1995, 1944, 2005, 1988];
-
-function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
-      arrRes.push(fn(arr[i]));
+function interviewQuestion(job) {
+    if (job === 'designer') {
+      return function(name) {
+        console.log(name + ', can you please explain what UX design is?');
+      }
+    } else if (job === 'teacher') {
+        return function(name){
+        console.log(name + ', what do you teach?')
+        };
+    } else {
+      return function(name) {
+        console.log('Hello ' + name + ', what do you do?');
+      }
     }
-    return arrRes;
-}
-function calculateAge(el) {
-  return 2019 - el;
-};
-
-function isFullAge(el) {
-  return el >= 18;
-};
-
-function maxHeartrate(el) {
-
-  if (el >= 18 && el <= 81) {
-  return Math.round(206.9 - (0.67 * el));
-  } else {
-    return -1;
   }
-};
 
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartrate);
-console.log(ages);
-console.log(fullAges);
-console.log(rates);
+var teacherQuestion =
+interviewQuestion('teacher');
+var designerQuestion =
+interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
