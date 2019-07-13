fc5fd3165764b2136c212fb1b12942e8857d072e
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-13 11:46:54 +0100

    IIFE

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,27 +1,40 @@
 // functions returning functions
 
-function interviewQuestion(job) {
-    if (job === 'designer') {
-      return function(name) {
-        console.log(name + ', can you please explain what UX design is?');
-      }
-    } else if (job === 'teacher') {
-        return function(name){
-        console.log(name + ', what do you teach?')
-        };
-    } else {
-      return function(name) {
-        console.log('Hello ' + name + ', what do you do?');
-      }
-    }
-  }
+// function interviewQuestion(job) {
+//     if (job === 'designer') {
+//       return function(name) {
+//         console.log(name + ', can you please explain what UX design is?');
+//       }
+//     } else if (job === 'teacher') {
+//         return function(name){
+//         console.log(name + ', what do you teach?')
+//         };
+//     } else {
+//       return function(name) {
+//         console.log('Hello ' + name + ', what do you do?');
+//       }
+//     }
+//   }
 
-var teacherQuestion =
-interviewQuestion('teacher');
-var designerQuestion =
-interviewQuestion('designer');
+// var teacherQuestion =
+// interviewQuestion('teacher');
+// var designerQuestion =
+// interviewQuestion('designer');
 
-teacherQuestion('John');
-designerQuestion('John');
+// teacherQuestion('John');
+// designerQuestion('John');
 
-interviewQuestion('teacher')('Mark');
+// interviewQuestion('teacher')('Mark');
+
+// IIFE
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >=5);
+  })();
+
+(function(goodLuck) {
+  var score = Math.random() * 10;
+  console.log(score >=5 - goodLuck);
+  })(5);
+//data privacy
