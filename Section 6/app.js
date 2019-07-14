aa5796c4dbc7b7e9b40ef40e667abfba845902b9
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-14 14:04:08 +0100

    toggle red/green for income/expense

--- Section 6/app.js
+++ Section 6/app.js
@@ -171,6 +171,12 @@ var UIController = (function() {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
       };
 
+    var nodeListForEach = function(list, callback){
+        for (var i = 0; i < list.length; i++){
+          callback(list[i], i);
+        }
+      };
+
     return {
       getInput: function() {
         return {
@@ -248,12 +254,6 @@ var UIController = (function() {
 
         var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-        var nodeListForEach = function(list, callback){
-          for (var i = 0; i < list.length; i++){
-            callback(list[i], i);
-          }
-        };
-
         nodeListForEach(fields, function(current, index) {
           if (percentages[index] > 0) {
             current.textContent = percentages[index] + '%';
@@ -273,7 +273,21 @@ var UIController = (function() {
         month = now.getMonth();
 
         document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+      },
 
+      changedType: function() {
+
+        var fields = document.querySelectorAll(
+            DOMstrings.inputType + ', ' +
+            DOMstrings.inputDescription + ', ' +
+            DOMstrings.inputValue
+          );
+
+        nodeListForEach(fields, function(cur) {
+          cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
       },
 
       getDOMstrings: function() {
@@ -300,6 +314,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       });
 
       document.querySelector(DOM.container).addEventListener('click', ctrDeleteItem);
+
+      document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function () {
