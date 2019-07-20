14409899c50b7946fae66693eaf528514d5d6803
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-20 08:37:17 +0100

    maps

--- Section 7/script.js
+++ Section 7/script.js
@@ -342,20 +342,58 @@
 // }
 
 // ES6
-function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'English'){
+// function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'English'){
 
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.yearOfBirth = yearOfBirth;
-  this.nationality = nationality;
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.yearOfBirth = yearOfBirth;
+//   this.nationality = nationality;
+// }
+
+// var john = new SmithPerson('John', 1990);
+// console.log(john);
+
+// var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+// console.log(emily);
+
+/////////////////////////////////////////////
+
+// MAPS
+
+const question = new Map();
+question.set('question', 'What is the official name of the latest major JavaScript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct!');
+question.set(false, 'Wrong, try again.');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+// if(question.has(4)) {
+// //     question.delete(4);
+// }
+
+// question.clear();
+
+// question.forEach((value, key) =>
+//   console.log(`This is the key ${key} and it is set to ${value}`))
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
 }
 
-var john = new SmithPerson('John', 1990);
-console.log(john);
-
-var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
-console.log(emily);
-/////////////////////////////////////////////
-
+const ans =  parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
 
 /////////////////////////////////////////////
+
+/////////////////////////////////////////////
+
+/////////////////////////////////////////////
+
