e08fa26879a75f9e3433a3d4dfad198c8192948d
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-21 20:27:56 +0100

    installed Babel

--- Section 9/src/js/index.js
+++ Section 9/src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import x from './test';
+const num = 23;
 
-console.log(`I imported ${x} from another module!`);
+console.log(`I imported ${x} from another module! Var x is ${num}`);
