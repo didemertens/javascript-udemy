06fe090ee1436a6d092a49da0b1193fd4fcbbf0f
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-23 20:29:44 +0100

    search model

--- Section 9/src/js/index.js
+++ Section 9/src/js/index.js
@@ -1,15 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '3081938082134b4eeb44e1b101de27b9';
-    try {
-        const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
+const search = new Search('pizza');
+console.log(search)
 
-getResults('vegan');
+search.getResults();
