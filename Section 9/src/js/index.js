dbf4420568bacce906529f7572d9cf8ee6de5516
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-23 20:41:55 +0100

    search controller

--- Section 9/src/js/index.js
+++ Section 9/src/js/index.js
@@ -1,6 +1,27 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search)
+// Global state of the app (search object, current recipe, shopping list, likes)
+const state = {};
 
-search.getResults();
+const controlSearch = async () => {
+    // Get query from view
+    const query = 'pizza'; // LATER
+
+    if (query) {
+        // New search object and add to state
+        state.search = new Search(query);
+
+        // Prepare UI for results
+
+        // Search for recipes
+        await state.search.getResults();
+
+        // Render results on UI
+        console.log(state.search.result);
+    }
+}
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
--- Section 9/src/js/models/Search.js
+++ Section 9/src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
         try {
             const res = await axios(`${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
+            // console.log(this.result);
         } catch (error) {
             alert(error);
         }
