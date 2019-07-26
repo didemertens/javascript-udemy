c9eb722dd26862057f7d18fb905d61287abcaf82
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-26 20:52:08 +0100

    moved arrow image

--- Section 9/src/js/views/SearchView.js
+++ Section 9/src/js/views/SearchView.js
@@ -46,10 +46,10 @@ const renderRecipe = recipe => {
 // type is previous or next
 const createButton = (page, type) => `
         <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page -1 : page + 1}>
+            <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
             <svg class="search__icon">
                 <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
             </svg>
-            <span>Page ${type === 'prev' ? page -1 : page + 1}</span>
         </button>
 `;
 
