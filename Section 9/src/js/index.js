88e5d3a9854cb58e8617d916815aded3e08e331f
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-29 14:19:54 +0200

    highlighting selected recipe, converting amounts

--- Section 9/src/js/index.js
+++ Section 9/src/js/index.js
@@ -64,6 +64,9 @@ const controlRecipe = async () => {
     recipeView.clearRecipe();
     renderLoader(elements.recipe);
 
+    // Highlight selected search
+    if (state.search) searchView.highlightSelected(id);
+
     // Create new recipe object
     state.recipe = new Recipe(id);
 
--- Section 9/src/js/views/SearchView.js
+++ Section 9/src/js/views/SearchView.js
@@ -82,3 +82,11 @@ export const renderResults = (recipes, page = 1, resPerPage = 10) => {
     // Buttons
     renderButtons(page, recipes.length, resPerPage);
 };
+
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    })
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+}
