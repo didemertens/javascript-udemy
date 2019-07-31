c3b293fcf73e6fdbc8548228ad97ed740ef146f6
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-31 17:33:24 +0200

    setting up the server

--- Section 12 Node/index.js
+++ Section 12 Node/index.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
+const http = require('http');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
-console.log(laptopData);
+
+const server = http.createServer((req, res) => {
+    console.log('Someone did access the server')
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('listening for requests now')
+});
