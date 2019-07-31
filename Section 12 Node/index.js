58a301d08fb5d55f34b68080f06822867ca81692
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-31 18:17:40 +0200

    adding query id

--- Section 12 Node/index.js
+++ Section 12 Node/index.js
@@ -8,19 +8,21 @@ const laptopData = JSON.parse(json);
 const server = http.createServer((req, res) => {
 
     const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
     if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html'});
         res.end('This is the products page');
     } 
     
-    else if (pathName === '/laptop') {
+    else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end('This is the laptop page');
+        res.end(`This is the laptop page for laptop ${id}!`);
     } 
 
     else {
         res.writeHead(404, { 'Content-type': 'text/html'});
-        res.end('Path not found on the server');
+        res.end('URL not found on the server');
     }
 
 });
