4aec259cd0d905e1b93852577c7a64bfda686c68
Dide Mertens <d.g.j.mertens@gmail.com>
2019-07-31 18:38:30 +0200

    response with data, using templates

--- Section 12 Node/index.js
+++ Section 12 Node/index.js
@@ -17,7 +17,19 @@ const server = http.createServer((req, res) => {
     
     else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end(`This is the laptop page for laptop ${id}!`);
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            output = output.replace(/{%IMAGE%}/g, laptop.image);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%STORAGE%}/g, laptop.storage);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            res.end(output);
+        });
     } 
 
     else {
