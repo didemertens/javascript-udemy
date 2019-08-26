2b2d5d526c916094e68849332de398b19072ad05
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-26 14:52:43 +0100

    chaining

---
+++ Adv_JS/objects/Chaining/js/app.js
@@ -0,0 +1,18 @@
+
+let str = "The course starts in October, 2018. ";
+
+let fruit = ["apple", "pear", "peach"];
+
+let fruitStr = fruit.concat("orange", "kiwi").sort().join(" - ").toUpperCase();
+
+console.log(fruitStr);
+
+// let str1 = str.replace("2018","2019").toUpperCase().trim();
+
+// // let str1 = str.replace("2018", "2019");
+
+// // let str2 = str1.toUpperCase();
+
+// // let str3 = str2.trim();
+
+// console.log(str1);
