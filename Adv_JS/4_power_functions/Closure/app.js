18e71b4929f0e5b8967e8a508e649e2594f492a4
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-29 21:05:53 +0100

    JS closure

--- Adv_JS/4_power_functions/Closure/app.js
+++ Adv_JS/4_power_functions/Closure/app.js
@@ -1,10 +1,30 @@
-var funct1 = function() {
-    var a = 2,
-        b = 3;
+// var funct1 = function() {
+//     var a = 2,
+//         b = 3;
 
-    var funct2 = function() {
-        console.log(a + b);
+//     var funct2 = function() {
+//         console.log(a + b);
+//     };
+
+//     setTimeout(funct2, 2000);
+// };
+
+(function() {
+    var cnt = 0,
+        item1 = document.querySelector('#item1'),
+        item2 = document.querySelector('#item2');
+
+    var print = function() {
+        console.log(cnt);
     };
 
-    funct2();
-};
+    item1.addEventListener('click', function() {
+        cnt++;
+        print();
+    });
+
+    item2.addEventListener('click', function() {
+        cnt++;
+        print();
+    });
+})();
