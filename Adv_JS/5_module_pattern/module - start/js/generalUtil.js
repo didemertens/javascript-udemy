92c16146711df65785a776ac90112b72941aa426
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-31 14:35:12 +0100

    module pattern, diff file

---
+++ Adv_JS/5_module_pattern/module - start/js/generalUtil.js
@@ -0,0 +1,21 @@
+var UTIL = (function(gen){
+
+    /*
+    Other Utilities
+    */
+
+   var domReady = function(funct) {
+        doc.addEventListener('DOMContentLoaded', function(){ 
+            if (typeof funct === "function") {
+                funct();
+            }
+        }, false);
+    };
+
+    // Public
+
+    gen.domReady = domReady;
+
+    return gen;
+
+}(UTIL || {}));
