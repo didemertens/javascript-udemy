189b9a4c59ca811fa22794be418170c564e94379
Dide Mertens <d.g.j.mertens@gmail.com>
2019-08-31 17:21:37 +0100

    loading json file on local server

--- Adv_JS/6_JSON/Loading JSON - start/json/main.js
+++ Adv_JS/6_JSON/Loading JSON - start/json/main.js
@@ -1,4 +1,21 @@
 var MAINAPP = (function(app) {
-    
+    var jsonOjb = {};
+
+    var loadJSON = function(path) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType('application/json');
+        xobj.open('GET', path);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState === 4) {
+                app.jsonOjb = JSON.parse(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    };
+
+
+    // Public
+    app.jsonOjb = jsonOjb;
+    app.loadJSON = loadJSON;
     return app;
 })(MAINAPP || {});
